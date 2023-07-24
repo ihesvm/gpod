50f51b964311834f44fda7d6402a9d50a8a4db26
ihesvm <hesamfm@gmail.com>
2023-07-24 15:52:39 +0330

    updated main.go

--- main.go
+++ main.go
@@ -3,15 +3,75 @@ package main
 import (
  "fmt"
  "net/http"
+ "io"
+ "context"
+ "errors"
+ "net"
+ "io/ioutil"
 )
 
+const keyServerAddr = "serverAddr"
+
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("could not read body: %s\n", err)
+	}
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s, body:\n%s\n",
+		ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second,
+		body)
+	io.WriteString(w, "This is my website\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Printf("%s: got /hello requests\n", ctx.Value(keyServerAddr))
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		w.Header().Set("x-missing-field", "myName")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
+}
+
+
 func main() {
- http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "Hello, World!")
- })
- fmt.Println("server has started")
- err := http.ListenAndServe(":8080", nil)
- if err != nil {
-  panic(err)
- }	
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+
+	<-ctx.Done()
+
 }
